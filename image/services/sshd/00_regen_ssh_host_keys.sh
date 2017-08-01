effb949df2df1ed2813ad2670af18bbd367ec5b8
Joshua Wilson <jwilson@waystone.systems>
2017-08-01 17:19:04 -0400

    fixed sshd key regen and turned password auth off by default

--- image/services/sshd/00_regen_ssh_host_keys.sh
+++ image/services/sshd/00_regen_ssh_host_keys.sh
@@ -4,5 +4,7 @@ if [[ ! -e /etc/service/sshd/down && ! -e /etc/ssh/ssh_host_rsa_key ]] || [[ "$1
 	echo "No SSH host key available. Generating one..."
 	export LC_ALL=C
 	export DEBIAN_FRONTEND=noninteractive
-	dpkg-reconfigure openssh-server
+	ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
+    ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
+    ssh-keygen -f /etc/ssh/ssh_host_ecdsa_key -N '' -t ecdsa -b 521
 fi
