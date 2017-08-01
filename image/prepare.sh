1ef7fc1d264c05f5b607013e8627c5ec0d914e55
Joshua Wilson <jwilson@waystone.systems>
2017-08-01 17:17:28 -0400

    cleanup references to dpkg

--- image/prepare.sh
+++ image/prepare.sh
@@ -10,33 +10,6 @@ export INITRD=no
 mkdir -p /etc/container_environment
 echo -n no > /etc/container_environment/INITRD
 
-## Enable Ubuntu Universe, Multiverse, and deb-src for main.
-#sed -i 's/^#\s*\(deb.*main restricted\)$/\1/g' /etc/apt/sources.list
-#sed -i 's/^#\s*\(deb.*universe\)$/\1/g' /etc/apt/sources.list
-#sed -i 's/^#\s*\(deb.*multiverse\)$/\1/g' /etc/apt/sources.list
-#apt-get update
-
-## Fix some issues with APT packages.
-## See https://github.com/dotcloud/docker/issues/1024
-#dpkg-divert --local --rename --add /sbin/initctl
-#ln -sf /bin/true /sbin/initctl
-
-## Replace the 'ischroot' tool to make it always return true.
-## Prevent initscripts updates from breaking /dev/shm.
-## https://journal.paul.querna.org/articles/2013/10/15/docker-ubuntu-on-rackspace/
-### https://bugs.launchpad.net/launchpad/+bug/974584
-#dpkg-divert --local --rename --add /usr/bin/ischroot
-#ln -sf /bin/true /usr/bin/ischroot
-
-## apt-utils fix for Ubuntu 16.04
-#$yum_install apt-utils
-#
-### Install HTTPS support for APT.
-#$yum_install apt-transport-https ca-certificates
-
-## Install add-apt-repository
-#$yum_install software-properties-common
-
 ## Upgrade all packages.
 #apt-get dist-upgrade -y --no-install-recommends -o Dpkg::Options::="--force-confold"
 rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
--- image/services/cron/cron.sh
+++ image/services/cron/cron.sh
@@ -13,7 +13,5 @@ sed -i 's/^\s*session\s\+required\s\+pam_loginuid.so/# &/' /etc/pam.d/cron
 ## Remove useless cron entries.
 # Checks for lost+found and scans for mtab.
 rm -f /etc/cron.daily/standard
-rm -f /etc/cron.daily/upstart
-rm -f /etc/cron.daily/dpkg
 rm -f /etc/cron.daily/password
 rm -f /etc/cron.weekly/fstrim
